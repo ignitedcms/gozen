83f2b92fb7b1f940a3e82cb946c4d68e0ecbbe62
ignitedcms <iamthwee@gmail.com>
2024-04-17 16:23:04 +0100

    u

--- controllers/mail/mail.go
+++ controllers/mail/mail.go
@@ -5,6 +5,8 @@ import (
 	"gozen/models/users"
 	//"gozen/system/mail"
 	"gozen/system/rendering"
+	"gozen/system/formutils"
+	"gozen/system/validation"
 	"net/http"
 )
 
@@ -20,14 +22,29 @@ func MailView(w http.ResponseWriter, r *http.Request) {
 
 func SendMail(w http.ResponseWriter, r *http.Request) {
 
-	query, err := users.GetHash("foo@mail.com")
+	validator := &validation.Validator{}
+
+	validator.Required("email", r.FormValue("email")).
+		Email("email", r.FormValue("email"))
+
+	//email := r.FormValue("email")
+	email := r.FormValue("email")
+
+	postData := formutils.SetAndGetPostData(w, r)
+
+	if validator.HasErrors() {
+		formutils.HandleValidationErrors(w, r, validator, postData, "mail")
+		return
+	}
+
+	query, err := users.GetHash(email)
    //no record found
 	if err != nil {
 		fmt.Print(err)
 	} else {
 		fmt.Print(query.Email)
 
-      to := query.Email
+      //to := query.Email
       //mail.Test(to)
 	}
 }

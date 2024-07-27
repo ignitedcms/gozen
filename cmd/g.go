8d797a9376b67d5c130e08f05afef3770cd66d79
ignitedcms <iamthwee@gmail.com>
2024-07-27 11:11:42 +0100

    chore: fix pgsql insert last id

--- cmd/g.go
+++ cmd/g.go
@@ -895,60 +895,57 @@ func GenerateCRUDPgsql(structName string, table string, fields []StructField) st
 	}
 	builder.WriteString("}\n\n")
 
-	// Generate Insert function
-	builder.WriteString(fmt.Sprintf("// Insert inserts a new %s into the database\n", structName))
-	builder.WriteString(fmt.Sprintf("func Create("))
-	firstField := true
-	for _, field := range fields {
-		if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
-			if !firstField {
-				builder.WriteString(", ")
-			} else {
-				firstField = false
-			}
-			builder.WriteString(fmt.Sprintf("%s %s", strings.ToLower(field.Name), field.Type))
-		}
-	}
-	builder.WriteString(") (int64, error) {\n")
-	builder.WriteString("\tstmt, err := db.DB.Prepare(\"INSERT INTO " + table + "(")
-	var insertFields []string
-	for _, field := range fields {
-		if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
-			insertFields = append(insertFields, field.Name)
-		}
-	}
-	builder.WriteString(strings.Join(insertFields, ", "))
-	builder.WriteString(", created_at, updated_at) VALUES(")
-	for i := range insertFields {
-		builder.WriteString(fmt.Sprintf("$%d", i+1))
-		if i < len(insertFields)-1 {
-			builder.WriteString(", ")
-		}
-	}
-	builder.WriteString(fmt.Sprintf(", $%d, $%d)\")", len(insertFields)+1, len(insertFields)+2))
-	builder.WriteString("\n\tif err != nil {\n")
-	builder.WriteString("\t\treturn 0, err\n")
-	builder.WriteString("\t}\n")
-	builder.WriteString("\tdefer stmt.Close()\n")
-	builder.WriteString("\tresult, err := stmt.Exec(")
-	for i, field := range fields {
-		if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
-			builder.WriteString(strings.ToLower(field.Name))
-			if i < len(fields)-1 && (fields[i+1].Name != "created_at" && fields[i+1].Name != "updated_at") {
-				builder.WriteString(", ")
-			}
-		}
-	}
-	builder.WriteString(", time.Now(), time.Now())")
-	builder.WriteString("\n\tif err != nil {\n")
-	builder.WriteString("\t\treturn 0, err\n")
-	builder.WriteString("\t}\n")
-	builder.WriteString("\tlastInsertID, err := result.LastInsertId()\n")
-	builder.WriteString("\tif err != nil {\n")
-	builder.WriteString("\t\treturn 0, err\n")
-	builder.WriteString("\t}\n")
-	builder.WriteString("\treturn lastInsertID, nil\n")
-	builder.WriteString("}\n\n")
+   // Generate Insert function
+   builder.WriteString(fmt.Sprintf("// Insert inserts a new %s into the database\n", structName))
+   builder.WriteString(fmt.Sprintf("func Create("))
+   firstField := true
+   for _, field := range fields {
+       if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
+           if !firstField {
+               builder.WriteString(", ")
+           } else {
+               firstField = false
+           }
+           builder.WriteString(fmt.Sprintf("%s %s", strings.ToLower(field.Name), field.Type))
+       }
+   }
+   builder.WriteString(") (int64, error) {\n")
+   builder.WriteString("\tstmt, err := db.DB.Prepare(\"INSERT INTO " + table + "(")
+   var insertFields []string
+   for _, field := range fields {
+       if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
+           insertFields = append(insertFields, field.Name)
+       }
+   }
+   builder.WriteString(strings.Join(insertFields, ", "))
+   builder.WriteString(", created_at, updated_at) VALUES(")
+   for i := range insertFields {
+       builder.WriteString(fmt.Sprintf("$%d", i+1))
+       if i < len(insertFields)-1 {
+           builder.WriteString(", ")
+       }
+   }
+   builder.WriteString(fmt.Sprintf(", $%d, $%d) RETURNING id\")", len(insertFields)+1, len(insertFields)+2))
+   builder.WriteString("\n\tif err != nil {\n")
+   builder.WriteString("\t\treturn 0, err\n")
+   builder.WriteString("\t}\n")
+   builder.WriteString("\tdefer stmt.Close()\n")
+   builder.WriteString("\tvar lastInsertID int64\n")
+   builder.WriteString("\terr = stmt.QueryRow(")
+   for i, field := range fields {
+       if field.Name != "id" && field.Name != "created_at" && field.Name != "updated_at" {
+           builder.WriteString(strings.ToLower(field.Name))
+           if i < len(fields)-1 && (fields[i+1].Name != "created_at" && fields[i+1].Name != "updated_at") {
+               builder.WriteString(", ")
+           }
+       }
+   }
+   builder.WriteString(", time.Now(), time.Now()).Scan(&lastInsertID)")
+   builder.WriteString("\n\tif err != nil {\n")
+   builder.WriteString("\t\treturn 0, err\n")
+   builder.WriteString("\t}\n")
+   builder.WriteString("\treturn lastInsertID, nil\n")
+   builder.WriteString("}\n\n")
 
 	// Generate Update function
 	builder.WriteString(fmt.Sprintf("// Update updates an existing %s in the database\n", structName))
